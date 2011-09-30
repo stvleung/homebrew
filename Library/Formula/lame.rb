86367204e76ecd219250b5770bfe57bdf4b60256
Steven Leung <stvleung@gmail.com>
2011-09-30 10:58:36 -0700

    add universal binary support for lame

--- Library/Formula/lame.rb
+++ Library/Formula/lame.rb
@@ -5,8 +5,23 @@ class Lame < Formula
   url 'http://downloads.sourceforge.net/sourceforge/lame/lame-3.98.4.tar.gz'
   md5 '8e9866ad6b570c6c95c8cba48060473f'
 
+  def options
+  [
+    ['--universal', 'Build universal binaries.']
+  ]
+  end
+
   def install
-    system "./configure", "--disable-debug", "--prefix=#{prefix}", "--enable-nasm"
+    args = ["--disable-debug",
+            "--prefix=#{prefix}",
+            "--enable-nasm"]
+
+    if ARGV.build_universal?
+      ENV.universal_binary 
+      args << "--disable-dependency-tracking"
+    end
+
+    system "./configure", *args
     system "make install"
   end
 end
