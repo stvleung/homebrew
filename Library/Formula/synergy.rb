affbfc190209870bcb19b69839946aa8629681af
Steven Leung <stvleung@gmail.com>
2011-10-10 10:27:17 -0700

    Upgrade synergy to 1.4.4

--- Library/Formula/synergy.rb
+++ Library/Formula/synergy.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Synergy < Formula
 
   if ARGV.build_head?
-    url 'http://synergy.googlecode.com/files/synergy-1.4.3-MacOSX106-Universal.zip'
-    sha1 'aa60fa6ac975dd22dc095d6cd9f3dc755b91bb64'
-    version '1.4.3'
+    url 'http://synergy.googlecode.com/files/synergy-1.4.4-MacOSX106-Universal.zip'
+    sha1 'fb30cdeea870f6db1d1047515ceae389fd5d26d4'
+    version '1.4.4'
   else
     # Newer 1.3.x versions have a critical bug on OS X:
     # http://code.google.com/p/synergy-plus/issues/detail?id=47
