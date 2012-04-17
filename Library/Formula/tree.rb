1f210021979eb5f0e90ba8297eb39b24a01fe919
Steven Leung <stvleung@gmail.com>
2012-04-17 11:44:50 -0700

    Add option for universal binary for tree

--- Library/Formula/tree.rb
+++ Library/Formula/tree.rb
@@ -5,10 +5,16 @@ class Tree < Formula
   url 'http://mama.indstate.edu/users/ice/tree/src/tree-1.6.0.tgz'
   md5 '04e967a3f4108d50cde3b4b0e89e970a'
 
+  def options
+    [['--universal', 'Build universal binaries.']]
+  end
+
   def install
     ENV.append 'CFLAGS', '-fomit-frame-pointer'
     ENV.append 'CFLAGS', '-no-cpp-precomp' unless ENV.compiler == :clang
     objs = 'tree.o unix.o html.o xml.o hash.o color.o strverscmp.o'
+
+    ENV.universal_binary if ARGV.build_universal?
 
     system "make", "prefix=#{prefix}",
                    "MANDIR=#{man1}",
