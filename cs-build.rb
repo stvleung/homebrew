044bd74de573803bb2e006a0c7fedc500ec0dfa4
Steven Leung <stvleung@gmail.com>
2013-11-14 18:17:25 -0800

    Patch cs-build with Mac OSX compatible linker options

--- cs-build.rb
+++ cs-build.rb
@@ -7,7 +7,38 @@ class CsBuild < Formula
 
   head 'git://scm.codesynthesis.com/build/build.git'
 
+  def patches; DATA; end
+
   def install
     system "make", "install_prefix=#{prefix}", "install" 
   end
 end
+
+__END__
+diff --git a/build/c/gnu/o-l.make b/build/c/gnu/o-l.make
+index 5c93f0a..a059a86 100644
+--- a/build/c/gnu/o-l.make
++++ b/build/c/gnu/o-l.make
+@@ -64,7 +64,7 @@ ifeq ($(mingw),n)
+ #
+        $(call message,ld  $@,$(ld) -shared \
+ $(c_extra_options) $(ld_options) $(c_ld_extra_options) \
+--o $(@D)/lib$(basename $(@F)).so -Wl$(comma_)-soname=lib$(basename $(@F)).so \
++-o $(@D)/lib$(basename $(@F)).so -Wl$(comma_)-install_name,lib$(basename $(@F)).so \
+ $(foreach f,$^,$(if $(patsubst %.l,,$f),$f,$(call expand-l,$f))) $(c_extra_libs))
+        $(call message,,echo "$(@D)/lib$(basename $(@F)).so" >$@)
+        $(call message,,echo "rpath:$(@D)" >>$@)
+diff --git a/build/cxx/gnu/o-l.make b/build/cxx/gnu/o-l.make
+index ea3e1d1..7e75b6d 100644
+--- a/build/cxx/gnu/o-l.make
++++ b/build/cxx/gnu/o-l.make
+@@ -65,7 +65,7 @@ ifeq ($(mingw),n)
+ #
+        $(call message,ld  $@,$(ld) -shared \
+ $(cxx_extra_options) $(ld_options) $(cxx_ld_extra_options) \
+--o $(@D)/lib$(basename $(@F)).so -Wl$(comma_)-soname=lib$(basename $(@F)).so \
++-o $(@D)/lib$(basename $(@F)).so -Wl$(comma_)-install_name,lib$(basename $(@F)).so \
+ $(foreach f,$^,$(if $(patsubst %.l,,$f),$f,$(call expand-l,$f))) $(cxx_extra_libs))
+        $(call message,,echo "$(@D)/lib$(basename $(@F)).so" >$@)
+        $(call message,,echo "rpath:$(@D)" >>$@)
+
