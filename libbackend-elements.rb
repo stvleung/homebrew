b7ee944c0add825072b1e36d066661f52f330c65
Steven Leung <stvleung@gmail.com>
2013-11-19 18:06:45 -0800

    Add libbackend-elements 1.7.2

---
+++ libbackend-elements.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class LibbackendElements < Formula
+  homepage 'http://kolpackov.net/projects/libbackend-elements'
+  url 'ftp://kolpackov.net/pub/projects/libbackend-elements/1.7/libbackend-elements-1.7.2.tar.bz2'
+  sha1 '6658cbbe22c4a71c8db88fb4c3db690c1695fd1a'
+
+  depends_on 'cs-build' => :build
+
+  def install
+    mkdir "#{name}-apple-darwin"
+    chdir "#{name}-apple-darwin"
+    system "make", "-f", "../makefile", "install_prefix=#{prefix}"
+  end
+end
